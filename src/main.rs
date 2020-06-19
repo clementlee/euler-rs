11b0decde9249e3d75b20fbc1367d78211a13c92
Clement Lee <2158058+clementlee@users.noreply.github.com>
2020-06-18 22:52:42 -0400

    refactor runner just a bit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod problems;
 
+use std::env;
+
 fn main() {
-    println!("{}", problems::p1::solution());
-    println!("{}", problems::p2::solution());
-    println!("{}", problems::p3::solution());
+    let args: Vec<String> = env::args().collect();
+
+    let problem_num: u32 = args[1].parse().expect("specify a number pls");
+
+    match problem_num {
+        1 => println!("{}", problems::p1::solution()),
+        2 => println!("{}", problems::p2::solution()),
+        3 => println!("{}", problems::p3::solution()),
+        n => println!(":( bad value: {}", n),
+    }
 }
