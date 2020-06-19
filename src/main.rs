66d360e073075f8182da85d2e4ea393820d66ddc
Clement Lee <2158058+clementlee@users.noreply.github.com>
2020-06-18 22:29:22 -0400

    p2 complete!

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod problems;
 
 fn main() {
     println!("{}", problems::p1::solution());
+    println!("{}", problems::p2::solution());
 }
---
+++ src/problems/p2.rs
@@ -0,0 +1,23 @@
+pub fn solution() -> u32 {
+    let limit:u32 = 4_000_000;
+    
+    let mut i1:u32 = 1;
+    let mut i2:u32 = 1;
+
+    let mut sum:u32 = 0;
+
+    loop {
+        let temp = i2;
+        i2 += i1;
+        i1 = temp;
+
+        if i2 > limit {
+            break
+        }
+        else if i2 % 2 == 0 {
+            sum += i2
+        }
+    }
+
+    sum
+}
